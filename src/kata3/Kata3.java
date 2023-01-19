11d41561aa5f73204c7ece3959e19d2b0a483e70
Raúl <Ral@Marrero>
2023-01-19 10:32:11 +0000

    Version 1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,14 @@
+package kata3;
+
+/**
+ *
+ * @author Raúl Marrero Marichal
+ */
+public class Kata3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+    
+}
